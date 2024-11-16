e700fb97fe8f838e49eaa89b59ac3602fa483e0b
szeedneigh <sidneyjohn1704@gmail.com>
2024-11-16 13:51:45 +0800

    Add text input component

--- react-hooks/src/main.jsx
+++ react-hooks/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Counter from './components/Counter.jsx'
+import ToggleButton from './components/ToggleButton.jsx'
+import TextInput from './components/TextInput.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <TextInput />
+  </StrictMode>
 )
